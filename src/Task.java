36f444a9a142e63d9524e36838fd9d048bdd78e6
Snigdha Singhania <singhaniasnigdha@gmail.com>
2016-02-22 01:27:15 +0800

    Added set/get Date and Time methods to Task class. Also added variables to determine Task type, whether floating or scheduled.

--- src/Task.java
+++ src/Task.java
@@ -1,9 +1,13 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 
 public class Task {
 	
 	private String description;
-	private long startDate, endDate, dueDate;
-	private long startTime, endTime, dueTime;
+	private boolean isFloatingType, isScheduledType;
+	private LocalDate startDate, endDate, dueDate;
+	private LocalTime startTime, endTime, dueTime;
 	
 	public Task()  {
 		
@@ -12,5 +16,73 @@ public class Task {
 	public void setDescription(String description){
 		this.description = description;
 	}
-
+	
+	public String getDescription(){
+		return this.description;
+	}
+	
+	public void setFloatingTask(){
+		isFloatingType = true; 
+	}
+	
+	public void setScheduledTask(){
+		isFloatingType = false; 
+		isScheduledType = true;
+	}
+	
+	public boolean isFloatingTask() {
+		return isFloatingType;
+	}
+	
+	public boolean isScheduledTask() {
+		return isScheduledType;
+	}
+	
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate; 
+	}
+	
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate; 
+	}
+	
+	public void setDueDate(LocalDate dueDate) {
+		this.dueDate = dueDate; 
+	}
+	
+	public LocalDate getStartDate() {
+		return this.startDate; 
+	}
+	
+	public LocalDate getEndDate() {
+		return this.endDate; 
+	}
+	
+	public LocalDate getDueDate() {
+		return this.dueDate; 
+	}
+	
+	public void setStartTime(LocalTime startTime) {
+		this.startTime = startTime; 
+	}
+	
+	public void setEndTime(LocalTime endTime) {
+		this.endTime = endTime; 
+	}
+	
+	public void setDueTime(LocalTime dueTime) {
+		this.dueTime = dueTime; 
+	}
+	
+	public LocalTime getStartTime() {
+		return this.startTime; 
+	}
+	
+	public LocalTime getEndTime() {
+		return this.endTime; 
+	}
+	
+	public LocalTime getDueTime() {
+		return this.dueTime; 
+	}
 }
