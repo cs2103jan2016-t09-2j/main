c27d39657fd64649b359c81d6bf4b347c28f8892
jonosim <jsimzw@hotmail.sg>
2016-02-28 17:24:58 +0800

    junit test for storage

---
+++ src/Storage/StorageTest.java
@@ -0,0 +1,20 @@
+package storage;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import java.util.ArrayList;
+
+public class StorageTest {
+
+	@Test
+	public void testWriteToFile() throws NullPointerException {
+		Storage data = new Storage();
+		ArrayList<Task> taskList = new ArrayList<>();
+
+		taskList.add();
+		taskList.add();
+		taskList.add();
+	}
+
+	}
