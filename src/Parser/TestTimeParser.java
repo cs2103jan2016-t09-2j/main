c40d1eac2aca1fc5c562d9f0c1e7917317748d56
Snigdha Singhania <singhaniasnigdha@gmail.com>
2016-02-29 00:19:27 +0800

    Added 4 unit tests to TestTimeParser.java

--- src/Parser/TestTimeParser.java
+++ src/Parser/TestTimeParser.java
@@ -3,5 +3,57 @@ package Parser;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+
 public class TestTimeParser { 
+	
+	TimeParser obj = new TimeParser();
+	
+	@Test
+	public void testFindTime1(){
+		String output = "";
+		String testString = "Meet ABCD at 16:00 on 14/05/1234";
+		ArrayList<String> outList = obj.findTime(testString);
+		for(String s : outList ) {
+			output = output + s; 
+		}
+		String expected = "16:00";
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void testFindTime2(){
+		String output = "";
+		String testString = "Meet ABCD 16.00";
+		ArrayList<String> outList = obj.findTime(testString);
+		for(String s : outList ) {
+			output = output + s; 
+		}
+		String expected = "16.00";
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void testFindTime3(){
+		String output = "";
+		String testString = "Meet ABCD at 14:00 15.00  ";
+		ArrayList<String> outList = obj.findTime(testString);
+		for(String s : outList ) {
+			output = output + s; 
+		}
+		String expected = "14:0015.00";
+		assertEquals(expected, output);
+	}
+	
+	@Test
+	public void testFindTime4(){
+		String output = "";
+		String testString = "Meet ABCD 14/05/1234";
+		ArrayList<String> outList = obj.findTime(testString);
+		for(String s : outList ) {
+			output = output + s; 
+		}
+		String expected = "";
+		assertEquals(expected, output);
+	}
 }
