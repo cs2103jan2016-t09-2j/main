36ed48b9f2ec6cb26f7751077c582205bc7a9f16
Snigdha Singhania <singhaniasnigdha@gmail.com>
2016-02-28 20:23:31 +0800

    Added DateTimeParser and TestDateTiemParser classes. Still incomplete though. Working on it now.

---
+++ src/Parser/DateTimeParser.java
@@ -0,0 +1,28 @@
+package Parser;
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * This class contains all possible combinations of date and time,
+ * that the user can input as command into Schedule Hacks.
+ * 
+ * @author Snigdha Singhania
+ */
+class DateTimeParser {
+	
+	private static final String REGEX_DATE = "";
+	private static final String REGEX_TIME = "";
+	
+	private static final String REGEX_EXTRA_WHITESPACE = "\\s{2,}";
+	
+	static String cleanup(String someText) {
+		Pattern extraSpace = Pattern.compile(REGEX_EXTRA_WHITESPACE);
+		Matcher regexMatcher = extraSpace.matcher(someText.trim());
+		String cleanText = regexMatcher.replaceAll(" ");
+		return cleanText;
+	}
+}
