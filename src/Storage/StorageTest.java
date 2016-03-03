b1f96577801599379cdbffc2347a2c2d249ae1ad
Snigdha Singhania <singhaniasnigdha@gmail.com>
2016-03-03 10:55:22 +0800

    Latest. Jon you need to UnComment your file. Sorry.

--- src/Storage/StorageTest.java
+++ src/Storage/StorageTest.java
@@ -1,22 +1,22 @@
-package Storage;
-
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-import java.util.ArrayList;
-
-import ScheduleHacks.Task;
-
-public class StorageTest {
-
-	@Test
-	public void testWriteToFile() throws NullPointerException {
-		Storage data = new Storage();
-		ArrayList<Task> taskList = new ArrayList<>();
-
-		//taskList.add();
-		//taskList.add();
-		//taskList.add();
-	}
-
-	}
+//package Storage;
+//
+//import static org.junit.Assert.*;
+//
+//import org.junit.Test;
+//import java.util.ArrayList;
+//
+//import ScheduleHacks.Task;
+//
+//public class StorageTest {
+//
+//	@Test
+//	public void testWriteToFile() throws NullPointerException {
+//		Storage data = new Storage();
+//		ArrayList<Task> taskList = new ArrayList<>();
+//
+//		//taskList.add();
+//		//taskList.add();
+//		//taskList.add();
+//	}
+//
+//	}
