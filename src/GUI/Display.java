5ad558669a5bbaf38a504388a263c5d980c292d0
lohchuengio <lohchuengio@gmail.com>
2016-03-03 09:55:56 +0800

    Added files via upload
    Add basic display layout

---
+++ src/GUI/Display.java
@@ -0,0 +1,59 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Display extends Application{
+
+
+	    Stage window;
+
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
+
+	    @Override
+	    public void start(Stage primaryStage) throws Exception {
+	        window = primaryStage;
+	        window.setTitle("Basic Display");
+
+	        //GridPane with 10px padding around edge
+	        GridPane grid = new GridPane();
+	        grid.setPadding(new Insets(10, 10, 10, 10));
+	        grid.setVgap(8);
+	        grid.setHgap(10);
+
+	        //Name Label - constrains use (child, column, row)
+	        Label commandLabel = new Label("Command");
+	        GridPane.setConstraints(commandLabel, 0, 1);
+	        
+	        Label displaySLabel = new Label("Schedule");
+	        GridPane.setConstraints(displaySLabel, 0, 0);
+	        
+	        Label displayFLabel = new Label("Floating");
+	        GridPane.setConstraints(displayFLabel, 3, 0);
+
+	        //Name Input
+	        TextField nameInput = new TextField("<Command>");
+	        GridPane.setConstraints(nameInput, 0, 15);
+
+	 
+
+
+
+
+
+	        //Add everything to grid
+	        grid.getChildren().addAll(displaySLabel,displayFLabel, nameInput);
+
+	        Scene scene = new Scene(grid, 300, 200);
+	        window.setScene(scene);
+	        window.show();
+	    }
+
+
+}
