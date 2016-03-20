303878ed74a0800eb3dbfafa5bb8a6c716c56c00
jonosim <jsimzw@hotmail.sg>
2016-03-20 15:55:13 +0800

    add test case in storageTest

--- src/Storage/StorageTest.java
+++ src/Storage/StorageTest.java
@@ -20,19 +20,28 @@ public class StorageTest {
 		Storage storageTest = Storage.getInstance();
 
 		Task test1 = new Task("attend soccer practice", null, null, null, null);
-		Task test2 = new Task("go to the gym", null, null, null, null);
+	//	Task test2 = new Task("go to the gym", null, null, null, null);
 		
 		ArrayList<Task> floatingTasksToDo = new ArrayList<Task>();
 		ArrayList<Task> scheduledTasksToDo = new ArrayList<Task>();
 		ArrayList<Task> scheduledTasksOverDue = new ArrayList<Task>();
+		
 		floatingTasksToDo.add(test1);
-	//	floatingTasksToDo.add(test2);
 		
 		storageTest.writeToCurrentFile(scheduledTasksToDo,floatingTasksToDo,scheduledTasksOverDue);
 		storageTest.readFromCurrentFile(scheduledTasksToDo,floatingTasksToDo,scheduledTasksOverDue);
 		
+		ArrayList<String> testList1 = new ArrayList<String>();
+		
+		testList1.add("attend soccer practice");
+		
+	//	assertEquals(testList1,storageTest.readFromCurrentFile(scheduledTasksToDo,floatingTasksToDo,scheduledTasksOverDue));
 		
 		
 	}
+
+
+
+
 
 }
