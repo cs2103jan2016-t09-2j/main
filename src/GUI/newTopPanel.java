774936c56038a16aec9ce93e6aff74cb9f32333d
CG <lohchuengio@gmail.com>
2016-03-24 05:17:55 +0800

    scroll bar added

--- src/GUI/newTopPanel.java
+++ src/GUI/newTopPanel.java
@@ -6,7 +6,10 @@ import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+
+import ScheduleHacks.Task;
 
 public class newTopPanel extends JPanel{
 
@@ -14,35 +17,47 @@ public class newTopPanel extends JPanel{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private static int count = 1;
+	private static boolean isNotEmpty = false;
 
 	private static JTextArea textArea;
+	private JScrollPane scrollPane;
+	
 	public newTopPanel(){
 		Dimension size = getPreferredSize();
 		size.height = 270;
 		setPreferredSize(size);
 		setBorder(BorderFactory.createTitledBorder(""));
-		
+
 		setLayout(new GridLayout());
 		textArea = new JTextArea();
+		scrollPane = new JScrollPane(textArea);
 		textArea.setEditable(false);
-		add(textArea);
+		add(scrollPane);
 	}
 
 	public static void setText(String string) {
 		textArea.append(string + "\n");
 		textArea.append("Bye");
 	}
-	
-	public static <E> void setText(ArrayList<E> OList, ArrayList<E> SList,ArrayList<E> FList){
+
+	public static void setText(ArrayList<Task> OList, ArrayList<Task> SList,ArrayList<Task> FList){
 		printOut(OList);
 		printOut(SList);
 		printOut(FList);
 	}
-	
-	public static <E> void printOut(ArrayList <E> List){
-		for(int i = 0; i <List.size(); i++){
-			String string = (String) List.get(i);
-			textArea.append(string + "\n");
+
+	public static void printOut(ArrayList<Task> List){
+		if(isNotEmpty){
+			textArea.setText(null);
+			count = 1;
 		}
+		for (Task task : List) {
+			isNotEmpty = true;
+			String string = task.getDescription();
+			textArea.append(count + ". " + string + "\n");
+			count++;
+		}
+
 	}
 }
