caae09788a53d0cb8d303332d631eddbdd4a355c
jonosim <jsimzw@hotmail.sg>
2016-03-28 13:19:01 +0800

    creating helpList

---
+++ src/ScheduleHacks/helpGuide.java
@@ -0,0 +1,75 @@
+package ScheduleHacks;
+
+import java.util.ArrayList;
+
+public class helpGuide {
+	
+	ArrayList<String> addList = new ArrayList<String>();
+	ArrayList<String> deleteList = new ArrayList<String>();
+	ArrayList<String> editList = new ArrayList<String>();
+	ArrayList<String> searchList = new ArrayList<String>();
+	ArrayList<String> undoAndRedoList = new ArrayList<String>();
+	ArrayList<String> completeList = new ArrayList<String>();
+	ArrayList<String> exitList = new ArrayList<String>();
+	
+	public ArrayList<String> getAddList() {
+		return addList;
+	}
+
+	public ArrayList<String> getDeleteList() {
+		return deleteList;
+	}
+
+	public ArrayList<String> getEditList() {
+		return editList;
+	}
+
+	public ArrayList<String> getSearchList() {
+		return searchList;
+	}
+
+	public ArrayList<String> getUndoAndRedoList() {
+		return undoAndRedoList;
+	}
+	public ArrayList<String> getCompleteList() {
+		return completeList;
+	}
+	
+	public ArrayList<String> getExitList() {
+		return exitList;
+	}
+	
+	public void addGuide() {
+
+	}
+
+	public void deleteGuide() {
+
+	}
+
+	public void editGuide() {
+
+	}
+
+	public void searchGuide() {
+
+	}
+
+	public void undoAndRedoGuide() {
+
+	}
+
+	public void completeGuide() {
+
+	}
+
+	public void exitGuide() {
+
+	}
+	
+	public void collateGuide(){
+		
+	}
+	
+	
+}
