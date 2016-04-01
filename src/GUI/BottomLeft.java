f78cf76ed531c6f197452dd098e149e1690203b6
CG <lohchuengio@gmail.com>
2016-04-01 19:39:22 +0800

    cleaning of code

---
+++ src/GUI/BottomLeft.java
@@ -0,0 +1,62 @@
+package GUI;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+
+public class BottomLeft extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private static BottomTop bottomTop;
+	private BottomBottom bottomBottom;
+	String text;
+	
+	public BottomLeft(){
+		Dimension size = getPreferredSize();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double height = screenSize.getHeight();
+		size.height = (int)(height/7.2);
+		setPreferredSize(size);
+		setBorder(BorderFactory.createLineBorder(Color.black));
+		setLayout(new GridBagLayout());
+		GridBagConstraints gc = new GridBagConstraints();
+
+		bottomTop = new BottomTop();
+		bottomBottom = new BottomBottom();
+
+		gc.fill = GridBagConstraints.BOTH;
+
+		gc.weightx = 1;
+		gc.weighty = 3;
+		gc.gridx = 0;
+		gc.gridy = 0;
+		add(bottomTop,gc);
+
+		gc.weighty = 1;
+		gc.gridx = 0;
+		gc.gridy = 3;
+		add(bottomBottom,gc);
+
+		this.setBackground(new Color(107, 179, 166));
+		//this.setBorder(null);
+	}
+
+	public String getText(){
+		System.out.println(text);
+		return text;
+	}
+
+	public static void setFeedback(String feedBack) {
+		bottomTop.setText(feedBack);
+	}
+}
+
---
+++ src/GUI/BottomRight.java
@@ -0,0 +1,51 @@
+package GUI;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Toolkit;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class BottomRight extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	JButton button1 = new JButton("TRIVAL");
+	JButton button2 = new JButton("UPCOMING");
+	JButton button3 = new JButton("ARCHIEVE");
+	JButton button4 = new JButton("HOME");
+	JButton button5 = new JButton("HELP");
+	
+	public BottomRight(){
+		Dimension size = getPreferredSize();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		double height = screenSize.getHeight();
+		size.height = (int)(height/7.2);
+		setPreferredSize(size);
+		setBorder(null);
+		setLayout(new GridBagLayout());
+		
+		GridBagConstraints gc = new GridBagConstraints();
+		gc.fill = GridBagConstraints.BOTH;
+
+		gc.weightx = 1;
+		gc.weighty = 1;
+		gc.gridx = 0;
+
+		gc.gridy = 0;
+		add(button1,gc);
+		gc.gridy = 1;
+		add(button2,gc);
+		gc.gridy = 2;
+		add(button3,gc);
+		gc.gridy = 3;
+		add(button4,gc);
+		gc.gridy = 4;
+		add(button5,gc);
+	}
+}
