82de69d1a1fbcdce41f87ec86b4f3d8e855c9d66
Snigdha Singhania <singhaniasnigdha@gmail.com>
2016-04-06 20:33:58 +0800

    Combining parser test cases

---
+++ src/Parser/ParserTestSuite.java
@@ -0,0 +1,15 @@
+package Parser;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  CommandParserTest.class,
+  DateParserTest.class,
+  TimeParserTest.class,
+  IndexParserTest.class,
+  DateTimeParserTest.class
+  
+})
+public class ParserTestSuite {   
+}  		
